99396eb011df9da36e301f29955053943b1ad1bd
Liel Harel <liel.harel@gmail.com>
2025-01-05 23:52:00 +0200

    PR fix

--- simple_text_analyzer/src/main.rs
+++ simple_text_analyzer/src/main.rs
@@ -14,15 +14,15 @@ fn main() {
             .read_line(&mut user_input)
             .expect("Failed to read line");
 
-        let operation_number: u32 = user_input
-            .trim()
-            .parse()
-            .expect("Did not enter a number of operation");
-
-        match operation_number {
-            1 => text_analyzer.get_text_to_analyze_from_user(),
-            2 => text_analyzer.print_user_word_freq(),
-            _ => println!("You enter invalid operation number, try again"),
+        match user_input.trim().parse::<u8>() {
+            Ok(operation_number) => match operation_number {
+                1 => text_analyzer.get_text_to_analyze_from_user(),
+                2 => text_analyzer.print_user_word_freq(),
+                _ => println!("You enter invalid operation number, try again"),
+            },
+            Err(err_msg) => {
+                println!("You enter invalid input {err_msg}. Please try again");
+            }
         }
     }
 }
